5e5d60edb4e990d3ee63aca3f594c4ee657280b7
Jakub Liput <jakub.liput@gmail.com>
2013-12-19 11:51:49 +0100

    Duży refactor kodu - utworzenie modułów

---
+++ server.py
@@ -0,0 +1,18 @@
+import sys
+from agents.server_agent import ServerAgent
+
+if __name__ == "__main__":
+    a = ServerAgent("message.server@"+sys.argv[1], "secret")
+    #a.wui.start()
+    a.setDebugToScreen()
+    a.start()
+    
+    try:
+        while True:
+            raw_input('')
+    except KeyboardInterrupt:
+        pass
+    
+    a.stop()
+    sys.exit(0)
+    
